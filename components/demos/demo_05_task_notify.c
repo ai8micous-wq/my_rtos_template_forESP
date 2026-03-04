3e9cbb6953c60f952af5face12ebfa0dbe620cab
ai8micous-wq <ai8micous@gmail.com>
2026-03-04 14:57:15 +0800

    Update demo_05_task_notify.c

--- components/demos/demo_05_task_notify.c
+++ components/demos/demo_05_task_notify.c
@@ -18,6 +18,8 @@
             ESP_LOGI(TAG, "notifier: give #%u", (unsigned)cnt);
         }
     }
+    // 每当notifier任务运行一次xTaskNotifyGive(worker);，那么通知计数就会+1
+    // 然后相应的任务（worker）就会马上被唤醒，进入Running状态。（如果没有被其他高优先级的任务抢占)
 
     static void worker_task(void *arg)
     {
@@ -29,10 +31,25 @@
             vTaskDelay(pdMS_TO_TICKS(100));
         }
     }
+    // ulTaskNotifyTake(pdTRUE, portMAX_DELAY); 这句话，按这两个参数的定义，如果没有被其他任务Notify，即通知计数为0
+    // 那么worker任务会进入阻塞状态，一直等待，直到被Notify唤醒。（重点：一直阻塞！！！）
+    // 其中ulTaskNotifyTake()返回，worker()处理任务过程中，被Notify的次数
+    // 由于notifier任务每300ms通知一次，worker执行耗时才100ms，所以got的值一直都是1
+    // 如果worker执行耗时大于notifier任务，那么got的值就会累积递增。
+    //
+    // 因此这个模型是一个非常轻量化的，任务间同步模型，适用于：
+    // 1. 比 Queue/Semaphore 更轻量、更快、内存更少
+    // 2. 非常适合 ISR 唤醒任务（例如 GPIO 中断、SPI DMA 完成中断）
+    // 3. 常用来做“事件到来→唤醒任务处理”的模型
+    // 例子：
+    //     GPIO 中断来了 → 通知采样任务读取一次
+    //     SPI DMA 完成 → 通知解析任务取 buffer
+    //     WiFi 连接成功 → 通知上传任务开始跑（也可用 EventGroup）
 
     void demo_05_run(void)
     {
         ESP_LOGI(TAG, "DEMO 05 start: Task Notification (lightweight signaling)");
         xTaskCreate(worker_task, "worker", 3072, NULL, 4, &worker);
+        // 第6个参数是任务句柄，用来保存创建任务成功后，读取存储它的句柄
         xTaskCreate(notifier_task, "notifier", 3072, NULL, 3, NULL);
     }
